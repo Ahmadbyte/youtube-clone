a618455218cb08c38576263620795550e04f2698
Ahmadbyte <shariqueahmad011@gmail.com>
2024-06-29 21:08:53 +0530

    .

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import './login.css'
+import './Login.css'
 
 const Login = () => {
   const [email, setEmail] = useState('');
